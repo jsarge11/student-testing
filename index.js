ac2a24e5802c448c740ef51889b0fbb65cf08a33
Jay Sargent <sargentjay11@gmail.com>
2019-02-13 07:09:47 -0700

    working version without demo

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 let express = require('express')
+const aws = require('aws-sdk');
+require('dotenv').config();
 
 let app = express();
 
-app.get('/test', (req, res) => {
+const {
+    S3_BUCKET,
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY
+} = process.env
 
- res.status(200).send();
-})
+app.get('/sign-s3', (req, res) => {
+
+  aws.config = {
+    region: 'us-west-1',
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY
+  }
+  
+  const s3 = new aws.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+
+    return res.send(returnData)
+  });
+});
 
 app.listen(4444, console.log('listening on 4444'))
--- src/App.js
+++ src/App.js
@@ -5,16 +5,61 @@ import axios from 'axios';
 
 class App extends Component {
   state = {
+    url: ''
+  }
+  getSignedRequest = (e) => {
+    let file = e.target.files[0];
 
+    axios.get('/sign-s3', {
+      params: {
+        'file-name': file.name,
+        'file-type': file.type
+      }
+    }).then( (response) => {
+      const { signedRequest, url } = response.data 
+      this.uploadFile(signedRequest, file, url)
+
+    }).catch( err => {
+      console.log(err)
+    })
+    
   }
-  handleChange = (e) => {
-    console.log(e.target.files)
-  }
+
+  uploadFile = (signedRequest, file, url) => {
+    const options = {
+      headers: {
+        'Content-Type': file.type,
+      },
+    };
+
+    axios
+    .put(signedRequest, file, options)
+    .then(response => {
+      this.setState({ isUploading: false, url });
+      // THEN DO SOMETHING WITH THE URL. SEND TO DB USING POST REQUEST OR SOMETHING
+    })
+    .catch(err => {
+      this.setState({
+        isUploading: false,
+      });
+      if (err.response.status === 403) {
+        alert(
+          `Your request for a signed URL failed with a status 403. Double check the CORS configuration and bucket policy in the README. You also will want to double check your AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY in your .env and ensure that they are the same as the ones that you created in the IAM dashboard. You may need to generate new keys\n${
+            err.stack
+          }`
+        );
+      } else {
+        alert(`ERROR: ${err.status}\n ${err.stack}`);
+      }
+    });
+};
+  
   
   render() {
     return (
       <div className="App">
-       <input type="file" onChange={this.handleChange}/>
+       <input type="file" onChange={this.getSignedRequest}/>
+       <img src={this.state.url} />
       </div>
     );
   }
