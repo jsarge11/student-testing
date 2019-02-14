cff400d5dbcc4a82a0a96654bdc81d96e58fdeb7
Jay Sargent <sargentjay11@gmail.com>
2019-02-14 13:55:53 -0700

    grid-testing 1

--- src/App.js
+++ src/App.js
@@ -5,64 +5,16 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <div class="sidebar">
-          <p>Some information here</p>
-        </div>
-        <div className="main">
-          <header>
-            <a href="#" id="logo">ImportantCo</a>
-
-            <nav>
-              <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-              </ul>
-            </nav>
-          </header>
-          <section id="hero">
-            <div id="content">
-              <h1>Sensible Solutions</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
-            </div>
-            <img src="images/mountain.jpg" id="mountain" />
-          </section>
-          <section id="info">
-            <div id="content2">
-              <h2>Reinvention</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
-            </div>
-            <img src="images/vector-mountains.svg" id="mountain-vector" />
-          </section>
-          <ul id="features">
-            <li>
-              <i class="fa fa-id-card" aria-hidden="true"></i>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-            </li>
-            <li>
-              <i class="fa fa-id-card" aria-hidden="true"></i>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-            </li>
-            <li>
-              <i class="fa fa-id-card" aria-hidden="true"></i>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-            </li>
-            <li>
-              <i class="fa fa-id-card" aria-hidden="true"></i>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-            </li>
-            <li>
-              <i class="fa fa-id-card" aria-hidden="true"></i>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-            </li>
-          </ul>
-        </div>
-        <footer>
-        <p>My footer bar</p>
-        </footer>
-
+      <div class="wrapper">
+        <div class="box a">A</div>
+        <div class="box b">B</div>
+        <div class="box c">C</div>
+        <div class="box d">D</div>
+        <div class="box e">E</div>
+        <div class="box f">F</div>
+        <div class="box g">G</div>
+        <div class="box g">H</div>
+        <div class="box g">I</div>
       </div>
     )
   }
